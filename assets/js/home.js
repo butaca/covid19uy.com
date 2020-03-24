d11818e9dd1369c83b3ba2793e631aae0c2793c4
Juan Fornos <juanfornos@gmail.com>
2020-03-24 19:48:03 -0300

    extracted uruguay data to a json file next to the world one (to keep all the data in the same place and available to hugo too)

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,23 +1,10 @@
+import data from "../../data/uruguay.json";
+
 document.addEventListener("DOMContentLoaded", function (event) {
     main();
 });
 
 function main() {
-    //FIXME: get from somewhere instead of hardcoding the values
-    var data = [
-        { date: '2020-03-13', cases : 4 },
-        { date: '2020-03-14', cases : 6 },
-        { date: '2020-03-15', cases : 8 },
-        { date: '2020-03-16', cases : 29 },
-        { date: '2020-03-17', cases : 50 },
-        { date: '2020-03-18', cases : 79 },
-        { date: '2020-03-19', cases : 94 },
-        { date: '2020-03-20', cases : 110 },
-        { date: '2020-03-21', cases : 135 },
-        { date: '2020-03-22', cases : 158 },
-        { date: '2020-03-23', cases : 162 },
-    ]
-
     var cases =  data.map( function(el) { return el.cases });
     var deaths = data.map( function(el) { return el.deaths != undefined ? el.deaths : 0 });
     var dates = data.map(function(el) {
