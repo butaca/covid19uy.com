e5fbea519e72cd3cf8c14312db72204376d1dcb3
Juan Fornos <juanfornos@gmail.com>
2020-04-02 19:32:56 -0300

    added share section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faReddit } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faFacebook, faTwitter, faLinkedin, faEnvelope, faReddit, faWhatsapp);
+
 import './home';
+
+dom.watch();
