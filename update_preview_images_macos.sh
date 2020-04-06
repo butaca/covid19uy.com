5ad8d7d53feb2ac01576738a1cd4835401d08315
Juan Fornos <juanfornos@gmail.com>
2020-04-06 20:08:53 -0300

    tweaked preview cropping

--- update_preview_images_macos.sh
+++ update_preview_images_macos.sh
@@ -2,7 +2,7 @@
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=1200,630 --default-background-color=0 --hide-scrollbars http://localhost:1313
 sips -s format jpeg screenshot.png --out opengraph.jpg
 mv opengraph.jpg ./static/images/seo/
-sips -s format jpeg --cropToHeightWidth 600 1200 --cropOffset 32 0 -Z 1024 screenshot.png --out twitter_card.jpg
+sips -s format jpeg --cropToHeightWidth 600 1200 --cropOffset 52 0 -Z 1024 screenshot.png --out twitter_card.jpg
 mv twitter_card.jpg ./static/images/seo/
 /Applications/ImageOptim.app/Contents/MacOS/ImageOptim ./static/images/seo/*.jpg
 rm screenshot.png
