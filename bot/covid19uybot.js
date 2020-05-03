559a00526f352fb684d4b9b6dcbf6e6b8131fa95
Juan Fornos <juanfornos@gmail.com>
2020-05-02 21:04:54 -0300

    reconnect bot when losing connection and added more accounts to follow and replay to

--- bot/covid19uybot.js
+++ bot/covid19uybot.js
@@ -8,28 +8,30 @@ const T = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+const SINAE_USER_ID = "840325920";
+const MSP_USER_ID = "3305529657";
+const COM_PRESIDENCIA = "380961080";
+const OPS_OMS_URUGUAY = "1673800032";
+const LUIS_LACALLE_POU = "189861728";
+const ALVARO_DELGADO = "231870362";
+
+const FOLLOW_IDS = [SINAE_USER_ID, MSP_USER_ID, COM_PRESIDENCIA, OPS_OMS_URUGUAY, LUIS_LACALLE_POU, ALVARO_DELGADO];
+
 function reply(tweetIdStr) {
     T.post('statuses/update', {
         status: "En mi sitio pueden ver los datos por día, en 12 gráficas: https://covid19uy.com \n\n#QuedateEnCasa #CoronavirusUy #CoronavirusEnUruguay #COVID19Uruguay",
-        in_reply_to_status_id : tweetIdStr,
+        in_reply_to_status_id: tweetIdStr,
         auto_populate_reply_metadata: true
     });
 }
 
-const SINAE_USER_ID = "840325920";
-const MSP_USER_ID = "3305529657";
-
-const followIds = SINAE_USER_ID + "," + MSP_USER_ID;
-
-const stream = T.stream('statuses/filter', {
-    track: 'informe,visualizador',
-    follow: followIds
-});
-
-stream.on('data', tweet => {
-    if (tweet.user != null && tweet.user != undefined && tweet.retweeted_status == undefined && tweet.quoted_status == undefined && tweet.in_reply_to_status_id == undefined) {
-        console.log("https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str);
-        if (tweet.user.id_str == SINAE_USER_ID || tweet.user.id_str == MSP_USER_ID) {
+const onData = data => {
+    // filter out non tweet data
+    if (data.user) {
+        const tweet = data;
+        // filter out retweets and replies
+        if (tweet.retweeted_status == undefined && tweet.in_reply_to_status_id == undefined) {
+            console.log("https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str);
             var lowerCaseText = tweet.text.toLowerCase();
             if ((lowerCaseText.indexOf("informe") != -1 || lowerCaseText.indexOf("visualizador") != -1) && (lowerCaseText.indexOf("coronavirus") != -1 || lowerCaseText.indexOf("covid") != -1)) {
                 console.log("https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str);
@@ -37,4 +39,53 @@ stream.on('data', tweet => {
             }
         }
     }
-});
+};
+
+let stream = null;
+
+const RECONNECTION_WAIT_MIN = 2 * 1000;
+const RECONNECTION_WAIT_MAX = 120 * 1000;
+const RECONNECTION_CALM_WAIT = 20 * 1000;
+let reconnectionWait = RECONNECTION_WAIT_MIN;
+
+const reconnect = (calm) => {
+    if(calm) {
+        reconnectionWait = Math.max(reconnectionWait, RECONNECTION_CALM_WAIT);
+    }
+    process.nextTick(() => {
+        if(stream) {
+            stream.destroy();
+            stream = null;
+        }
+        setTimeout(reconnectionWait, createStream);
+        reconnectionWait = Math.min(reconnectionWait * 2, RECONNECTION_WAIT_MAX);
+    });
+}
+
+const onStart = () => {
+    reconnectionWait = RECONNECTION_WAIT_MIN;
+}
+
+const onEnd = () => {
+    reconnect(false);
+};
+
+const onError = (error) => {
+    reconnect(error.status === 420 || error.status === 429);
+}
+
+const createStream = () => {
+    stream = T.stream('statuses/filter', {
+        follow: FOLLOW_IDS.join(','),
+        tweet_mode: 'extended'
+    });
+    stream.on('start', onStart);
+    stream.on('data', onData);
+    stream.on('error', onError);
+    stream.on('end', onEnd);
+}
+
+createStream();
+
+
+
