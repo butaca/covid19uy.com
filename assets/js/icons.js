e25961d5e237f6f7f46afa9d85c782481f6fc360
Juan Fornos <juanfornos@gmail.com>
2020-06-25 20:53:44 -0300

    refactored icons

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,12 +1,7 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faReddit } from '@fortawesome/free-brands-svg-icons';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faLanguage } from '@fortawesome/free-solid-svg-icons';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 
 library.add(faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp, faEnvelope, faLanguage, faQuestionCircle);
