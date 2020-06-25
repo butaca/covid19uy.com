523ad6f2073654a3a410c5dd82469ce244b9b679
Juan Fornos <juanfornos@gmail.com>
2020-06-25 20:33:34 -0300

    removed build triggers

--- trigger_build_develop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -X POST -d {} https://api.netlify.com/build_hooks/5e879ef5ca94ba01e46847af
--- trigger_build_master.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -X POST -d {} https://api.netlify.com/build_hooks/5e7ce29182835a82f45d85d7
