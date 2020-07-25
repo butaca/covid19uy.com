b13817d9dea55c760193847c9dd157d0ec07129b
Juan Fornos <juanfornos@gmail.com>
2020-07-25 18:52:27 -0300

    updated repo url and icon

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,9 +1,9 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import { faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp, faBitbucket } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faLanguage } from '@fortawesome/free-solid-svg-icons';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp, faBitbucket, faEnvelope, faLanguage, faQuestionCircle);
+library.add(faFacebook, faTwitter, faLinkedin, faReddit, faWhatsapp, faGithub, faEnvelope, faLanguage, faQuestionCircle);
 
 dom.watch();
