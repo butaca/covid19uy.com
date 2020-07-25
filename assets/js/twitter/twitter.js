6d52112447977780b293e8286944906630e1e7dc
Juan Fornos <juanfornos@gmail.com>
2020-07-25 20:05:39 -0300

    tweaked tweets loading (preload N tweets, dynamically load the remaining ones)

--- assets/js/twitter/twitter.js
+++ assets/js/twitter/twitter.js
@@ -12,5 +12,40 @@ if (document.readyState === 'loading') {
 function onDOMLoaded() {
     burger();
     nfCookies();
-    TwitterWidgetsLoader.load();
+
+    TwitterWidgetsLoader.load(function () {
+        var tweetElements = document.getElementsByClassName("tweet");
+        var pendingTweets = [];
+        var loadingTweets = 0;
+        var maxLoadingTweets = 6;
+
+        for (var i = 0; i < tweetElements.length; ++i) {
+            var tweetElement = tweetElements[i];
+            var tweetId = tweetElement.getAttribute("data-tweet");
+            pendingTweets.push({
+                tweetId: tweetId,
+                element: tweetElement
+            });
+        }
+
+        function onTweetLoaded() {
+            loadingTweets--;
+            loadPendingTweets();
+        }
+
+        function loadPendingTweets() {
+            while (pendingTweets.length > 0 && loadingTweets <= maxLoadingTweets) {
+                var pendingTweet = pendingTweets[0];
+                pendingTweets.splice(0, 1);
+                loadingTweets++;
+                var prom = twttr.widgets.createTweet(pendingTweet.tweetId, pendingTweet.element, {
+                    conversation: "none",
+                    cards: "hidden"
+                });
+                prom.then(onTweetLoaded).catch(onTweetLoaded);
+            }
+        }
+
+        loadPendingTweets();
+    });
 }
