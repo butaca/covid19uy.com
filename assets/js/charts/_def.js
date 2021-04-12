c032817fcb7bcde5a3770c54b8073ed2f412f3b9
Juan Fornos <juanfornos@gmail.com>
2021-04-12 11:48:11 -0300

    remved unused script file

--- assets/js/charts/_def.js
+++
@@ -1,7 +0,0 @@
-import { createDefaultChartOptions, createMovingAverageDataset, MOVING_AVERAGE_DELTA, pointRadius, pointHoverRadius } from './util'
-
-function chart(chartData, lang) {
-   
-}
-
-export default chart;
