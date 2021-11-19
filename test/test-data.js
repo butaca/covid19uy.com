af25d23c549da0638c368f2f476674a32463ac38
Juan Fornos <juanfornos@gmail.com>
2021-11-19 19:30:09 -0300

    updated uruguay

--- test/test-data.js
+++ test/test-data.js
@@ -296,6 +296,10 @@ describe('Test data', function () {
                 totalDeaths -= 1;
             }
 
+            if (date.getTime() == new Date("2021-11-19" + DATE_DEFAULT_TIME).getTime()) {
+                totalDeaths -= 1;
+            }
+
             deathHistory.push({ date: date, deaths: totalDeaths });
 
             // a death was removed on 2021-08-23.
